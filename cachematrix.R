82a7f77f42c04aaf27408a254326d5702375792b
Anna M <95537547+yunnieman@users.noreply.github.com>
2022-01-12 10:34:16 -0500

    Fill out the empty R script
    Filled function spaces as per assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## makeCacheMatrix assignment
+## creates a matrix that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  # set value of matrix
+  set <- function(y){
+    # callback to value
+    x <<- y   
+    inv <<- NULL
+  }
+  # get values of matrix
+  get <- function() {x}
+  sInverse <- function(inverse) {inv <<- inverse}
+  # get values of inverse
+  gInverse <- function() {inv}
+  list(set = set, get = get, sInverse = sInverse, gInverse = gInverse)
 }
 
 
-## Write a short comment describing this function
+## computes the inverse of matrix returned by makeCacheMatrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  inv <- x$gInverse()
+  # check if inverse was made
+  if(!is.null(inv)){
+    message("retrieving the cached data")
+    return(inv)
+  }
+  mat <- x$get()
+  inv <- solve(mat, ...)
+  x$sInverse(inv)
+  inv #returning
 }
