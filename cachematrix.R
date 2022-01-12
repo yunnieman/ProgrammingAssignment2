ab5f66a5d75948c601b62850948badf8dc74540a
Anna M <95537547+yunnieman@users.noreply.github.com>
2022-01-12 10:20:36 -0500

    Input functions onto file
    Filled out makeCacheMatrix and cacheSolve matrix as per assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Programming assignment 2 for Week 3
+## Lexical Scoping and caching Inverses
 
-## Write a short comment describing this function
+## This function creates a matrix that can cache its inverse.
+## assuming the input matrix is invertible
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  # set value of matrix
+  set <- function(y){
+    # callback to value
+    x <<- y   
+    inv <<- NULL
+    }
+    # get values of matrix
+    get <- function() {x}
+    sInverse <- function(inverse) {inv <<- inverse}
+    # get values of inverse
+    gInverse <- function() {inv}
+    list(set = set, get = get, sInverse = sInverse, gInverse = gInverse)
 }
 
 
-## Write a short comment describing this function
+## computes the inverse of matrix returned by makeCacheMatrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+inv <- x$gInverse()
+  # check if inverse was made
+  if(!is.null(inv)){
+    message("retrieving the cached data")
+    return(inv)
+  }
+  mat <- x$get()
+  inv <- solve(mat, ...)
+  x$sInverse(inv)
+  inv #returning
 }
